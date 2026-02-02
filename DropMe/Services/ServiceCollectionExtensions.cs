9908334926b853e78443d186dc377dfc18508dab
kellyl <kellyl82@tcd.ie>
2026-02-02 16:21:40 +0000

    Created async and sync work thread work manager

--- DropMe/Services/ServiceCollectionExtensions.cs
+++ DropMe/Services/ServiceCollectionExtensions.cs
@@ -6,6 +6,8 @@ namespace DropMe.Services;
 
 public static class ServiceCollectionExtensions {
     public static void AddCrossPlatformServices(this IServiceCollection services) {
+        // Work scheduling (cross-platform)
+        services.AddSingleton<IWorkManager, WorkManager>();
         // Register cross platform services here
         services.AddTransient<MainViewModel>();
     }
