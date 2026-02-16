7d09c40a09fb2b28ba7be3819cd134b5b245a9c4
kellyl <kellyl82@tcd.ie>
2026-02-16 20:34:22 +0000

    Whoops, forgot to git track session and transfer page. CI/CD might pass now?

---
+++ DropMe/Views/Session.axaml.cs
@@ -0,0 +1,56 @@
+using Avalonia.Interactivity;
+using System;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using DropMe.ViewModels;
+
+namespace DropMe.Views;
+
+public partial class Session : UserControl {
+    public event EventHandler? BackRequested;
+
+    public Session() {
+        InitializeComponent();
+        AttachedToVisualTree += (_, _) => {
+            if (DataContext is MainViewModel vm)
+                vm.FileOfferDecisionUi = vm.RequestFileOfferDecisionAsync;
+        };
+        DetachedFromVisualTree += (_, _) => {
+            if (DataContext is MainViewModel vm)
+                vm.FileOfferDecisionUi = null;
+        };
+    }
+
+    private void InitializeComponent() {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    private async void BackButton_Click(object? sender, RoutedEventArgs e) {
+        if (DataContext is MainViewModel vm) {
+            await vm.StopSessionAsync(resetToReady: false, homeMessage: "Session ended.");
+            await vm.PrepareMainPageAsync(homeMessage: "Session ended.", regenerateQr: true);
+        }
+
+        BackRequested?.Invoke(this, EventArgs.Empty);
+    }
+
+    private async void SendFiles_Click(object? sender, RoutedEventArgs e) {
+        if (DataContext is MainViewModel vm)
+            await vm.SendFileAsync();
+    }
+
+    private async void ChooseDownloadFolder_Click(object? sender, RoutedEventArgs e) {
+        if (DataContext is MainViewModel vm)
+            await vm.ChooseDownloadFolderAsync();
+    }
+
+    private void AcceptIncomingFile_Click(object? sender, RoutedEventArgs e) {
+        if (DataContext is MainViewModel vm)
+            vm.AcceptPendingFileOffer();
+    }
+
+    private void RejectIncomingFile_Click(object? sender, RoutedEventArgs e) {
+        if (DataContext is MainViewModel vm)
+            vm.RejectPendingFileOffer();
+    }
+}
