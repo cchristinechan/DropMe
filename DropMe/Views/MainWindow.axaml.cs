b80e215b958371227b41c511b5194828bbbc9152
roberta1 <roberta1@tcd.ie>
2026-02-18 11:55:04 +0000

    Removed legacy code

--- DropMe/Views/MainWindow.axaml.cs
+++ DropMe/Views/MainWindow.axaml.cs
@@ -11,17 +11,6 @@ public partial class MainWindow : Window {
 
     public MainWindow() {
         InitializeComponent();
-
-        MainContent.Content = new TransferPage();
-    }
-
-    public void ShowSession() {
-        var session = new Session();
-        MainContent.Content = session;
-
-        session.BackRequested += (_, _) => {
-            MainContent.Content = new TransferPage();
-        };
     }
 
     private void InitializeComponent() {
