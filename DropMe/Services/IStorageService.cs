c33e49c6b9176833568956d8634286357ca17af4
roberta1 <roberta1@tcd.ie>
2026-02-27 12:40:52 +0000

    Added documentation to the storage service interface

--- DropMe/Services/IStorageService.cs
+++ DropMe/Services/IStorageService.cs
@@ -5,15 +5,31 @@ using Avalonia;
 namespace DropMe.Services;
 
 public interface IStorageService {
-    // Opens a dialog for the user to pick a downloads folder
+    /// <summary>
+    /// Opens a dialogue for the user to pick a downloads folder for dropme to use.
+    /// </summary>
+    /// <param name="visual">A visual for the dialogue to be linked to.</param>
     public Task PickDownloadsFolderAsync(Visual? visual);
-    // Opens a stream in the downloads folder to write a file to
+    /// <summary>
+    /// Opens a stream with write permissions into the set downloads folder.
+    /// </summary>
+    /// <param name="fileName">The name of the file within the downloads folder to be opened.</param>
+    /// <returns>The stream and associated path of the file opened.</returns>
     public (Stream, string)? OpenDownloadFileWriteStreamAsync(string fileName);
-    // Do NOT use this to create/open files, use OpenDownloadFileWriteStream to directly open a stream
-    // Maybe change to be an onDownloadDirectoryChanged event
+    /// <summary>
+    /// Gets a user friendly label of the downloads folder. This may or may not be a valid path so should not be used
+    /// to open files.
+    /// </summary>
+    /// <returns>A user friendly label of the downloads folder.</returns>
     public string? GetDownloadDirectoryLabel();
-    // Returns a read stream to the config file
+    /// <summary>
+    /// Opens a stream for reading to the backing storage of the config.
+    /// </summary>
+    /// <returns>A stream with read permissions for the backing storage of the config.</returns>
     public Stream ReadConfig();
-    // Truncates the config file and returns a write stream to the empty file
+    /// <summary>
+    /// Truncates any existing config file and returns a writable stream to this new empty file.
+    /// </summary>
+    /// <returns>A writable stream to an empty config file.</returns>
     public Stream WriteConfig();
 }
