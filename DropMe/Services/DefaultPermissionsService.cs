326c512888e266028bf54030308e072c4d7e1d8f
roberta1 <roberta1@tcd.ie>
2026-03-12 16:40:11 +0000

    Formatting

--- DropMe/Services/DefaultPermissionsService.cs
+++ DropMe/Services/DefaultPermissionsService.cs
@@ -4,14 +4,14 @@ public class DefaultPermissionsService : IPermissionsService {
     public bool HasCameraPermissions => true;
     public bool HasBluetoothPermissions => true;
     public void RequestCameraPermission() {
-        
+
     }
 
     public void RequestBluetoothPermission() {
-        
+
     }
 
     public void RequestBluetoothDiscoverablePermission(int maxDuration) {
-         
+
     }
 }
