bbf5d53f6842af6dcda3de087469e91050e8934e
roberta1 <roberta1@tcd.ie>
2026-03-16 12:14:12 +0000

    Manually manage bluez dbus connection centrally to allow client and server to use the same profiles to prevent bluez whinging about registering a service twice

--- libs/32feet/InTheHand.Net.Bluetooth/Platforms/Linux/BluetoothListener.linux.cs
+++ libs/32feet/InTheHand.Net.Bluetooth/Platforms/Linux/BluetoothListener.linux.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using InTheHand.Net.Bluetooth.Platforms.Linux;
 using Tmds.DBus;
 
 namespace InTheHand.Net.Sockets {
@@ -23,15 +24,8 @@ namespace InTheHand.Net.Sockets {
         public string ServiceName { get; set; }
         public ServiceRecord ServiceRecord { get; set; }
         public Guid ServiceUuid { get; set; }
-        private BluetoothSdpServer _sdpServer;
         public void Start() {
-            // Validate that the object path is a valid dbus object path
-            var objPathEnd = string.IsNullOrEmpty(ServiceName) ? Process.GetCurrentProcess().Id.ToString() : ServiceName;
-            var dbusObjectPath = $"/com/32feet/listener/{objPathEnd}";
-            _sdpServer = new BluetoothSdpServer(dbusObjectPath, ServiceName);
-            _sdpServer.OnClientConnected += OnClientConnected;
-            Task.Run(async () => await _sdpServer.StartAsync(ServiceUuid)).Wait();
-            Console.WriteLine($"Sdp server advertising {ServiceUuid} using dbus object {dbusObjectPath} for bluez communication");
+            BluezProfile1Manager.Instance.OnServerConnected += OnClientConnected;
             Active = true;
         }
 
@@ -39,6 +33,7 @@ namespace InTheHand.Net.Sockets {
             if (Active) {
                 (_socketTcs.Task.GetAwaiter().GetResult()).Close();
                 _socketTcs = new TaskCompletionSource<LinuxSocket>();
+                BluezProfile1Manager.Instance.OnServerConnected -= OnClientConnected;
                 Active = false;
             }
         }
@@ -64,88 +59,10 @@ namespace InTheHand.Net.Sockets {
             return new BluetoothClient(new LinuxBluetoothClient((LinuxSocket)await AcceptSocketAsync()));
         }
 
-        private void OnClientConnected(Socket clientSocket) {
-            if (!_socketTcs.TrySetResult((LinuxSocket)clientSocket)) {
+        private void OnClientConnected(string clientAddr, LinuxSocket clientSocket) {
+            if (!_socketTcs.TrySetResult(clientSocket)) {
                 Console.WriteLine($"Dropping socket {clientSocket} as one has already been accepted");
             }
         }
-    }
-
-    [DBusInterface("org.bluez.Profile1")]
-    public interface IProfile1 : IDBusObject {
-        Task NewConnectionAsync(ObjectPath device, CloseSafeHandle fd, IDictionary<string, object> properties);
-        Task RequestDisconnectionAsync(ObjectPath device);
-        Task ReleaseAsync();
-    }
-
-    [DBusInterface("org.bluez.ProfileManager1")]
-    public interface IProfileManager1 : IDBusObject {
-        Task RegisterProfileAsync(ObjectPath profile, string uuid, IDictionary<string, object> options);
-        Task UnregisterProfileAsync(ObjectPath profile);
-    }
-
-    public class BluetoothSdpServer(string objPath, string serviceName) : IProfile1 {
-        public delegate void ClientConnectedHandler(Socket clientSocket);
-        public event ClientConnectedHandler OnClientConnected;
-        public ObjectPath ObjectPath { get; } = new(objPath);
-        private IProfileManager1 _profileManager;
-
-        public async Task StartAsync(Guid guid) {
-
-            var connection = new Connection("unix:path=/var/run/dbus/system_bus_socket");
-            await connection.ConnectAsync().ConfigureAwait(false);
-
-            await connection.RegisterObjectAsync(this).ConfigureAwait(false);
-
-            _profileManager = await Task.Run(() => connection.CreateProxy<IProfileManager1>("org.bluez", "/org/bluez"))
-                .WaitAsync(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
-            string service;
-            if (string.IsNullOrEmpty(serviceName)) {
-                service = "32feet sdp service";
-            }
-            else {
-                service = serviceName;
-            }
-
-            Console.WriteLine($"Guid: {guid}");
-            var sdpOptions = new Dictionary<string, object>
-            {
-                { "Name", service },
-                { "Role", "Server" },
-                { "Channel", (ushort)0 },
-                { "Service", guid.ToString() },
-            };
-
-            Console.WriteLine($"Registering SDP Profile for UUID {guid}");
-            await _profileManager.RegisterProfileAsync(ObjectPath, guid.ToString(), sdpOptions);
-            Console.WriteLine("SDP Profile registered successfully. Waiting for connections...");
-        }
-
-        public async Task StopAsync() {
-            await _profileManager.UnregisterProfileAsync(ObjectPath);
-            Console.WriteLine("Successfully unregistered SDP profile");
-        }
-
-        public Task NewConnectionAsync(ObjectPath device, CloseSafeHandle fd, IDictionary<string, object> properties) {
-            Console.WriteLine($"New Connection from device: {device}");
-            try {
-                var handle = fd.DangerousGetHandle();
-
-                var socket = new LinuxSocket(handle.ToInt32());
-
-                OnClientConnected?.Invoke(socket);
-            }
-            catch (Exception ex) {
-                Console.WriteLine($"Error wrapping Bluetooth socket: {ex.Message}");
-            }
-
-            return Task.CompletedTask;
-        }
-
-        public Task ReleaseAsync() {
-            return Task.CompletedTask;
-        }
-
-        public Task RequestDisconnectionAsync(ObjectPath device) => Task.CompletedTask;
     }
 }
---
+++ libs/32feet/InTheHand.Net.Bluetooth/Platforms/Linux/BluezProfile1Manager.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using InTheHand.Net.Sockets;
+using Tmds.DBus;
+
+namespace InTheHand.Net.Bluetooth.Platforms.Linux;
+
+public class BluezProfile1Manager {
+    private static Profile1 _instance;
+    private static readonly object _lock = new object();
+    private const string ServiceGuid = "bc8659c9-3aa7-4faf-ba42-c5feb93d1a3e";
+    private BluezProfile1Manager() { }
+
+    public static Profile1 Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Profile1("DropMe", new Guid(ServiceGuid));
+                    _instance.StartAsync().RunSynchronously();
+                }
+                return _instance;
+            }
+        }
+    }
+}
+
+[DBusInterface("org.bluez.Profile1")]
+public interface IProfile1 : IDBusObject {
+    Task NewConnectionAsync(ObjectPath device, CloseSafeHandle fd, IDictionary<string, object> properties);
+    Task RequestDisconnectionAsync(ObjectPath device);
+    Task ReleaseAsync();
+}
+
+[DBusInterface("org.bluez.ProfileManager1")]
+public interface IProfileManager1 : IDBusObject {
+    Task RegisterProfileAsync(ObjectPath profile, string uuid, IDictionary<string, object> options);
+    Task UnregisterProfileAsync(ObjectPath profile);
+}
+
+public class Profile1(string serviceName, Guid serviceGuid) : IProfile1 {
+    public ObjectPath ObjectPath => new ObjectPath($"/com/32feet/{Process.GetCurrentProcess().Id}");
+    private IProfileManager1 _profileManager;
+    private readonly Guid _serviceGuid = serviceGuid;
+    private readonly string _serviceName = serviceName;
+    private readonly Dictionary<string, ConnectionReceived> _clientConnectedCallbacks = new();
+    public event ConnectionReceived OnServerConnected;
+
+    public delegate void ConnectionReceived(string macAddr, LinuxSocket socket);
+
+    public void RegisterOnClientConnected(BluetoothAddress serverAddress, ConnectionReceived callback) {
+        _clientConnectedCallbacks.Add(serverAddress.ToString("C"), callback);
+    }
+    
+    public void UnregisterOnClientConnected(BluetoothAddress serverAddress) {
+        _clientConnectedCallbacks.Remove(serverAddress.ToString("C"));
+    }
+    
+    public async Task StartAsync() {
+        var connection = new Connection("unix:path=/var/run/dbus/system_bus_socket");
+        await connection.ConnectAsync().ConfigureAwait(false);
+
+        await connection.RegisterObjectAsync(this).ConfigureAwait(false);
+
+        _profileManager = await Task.Run(() => connection.CreateProxy<IProfileManager1>("org.bluez", "/org/bluez"))
+            .WaitAsync(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+        
+        var sdpOptions = new Dictionary<string, object>
+        {
+            { "Name", _serviceName },
+            { "Role", "Dual" },
+            { "Channel", (ushort)0 },
+            { "Service", _serviceGuid.ToString() },
+        };
+
+        Console.WriteLine($"Registering SDP Profile for UUID {_serviceGuid}");
+        await _profileManager.RegisterProfileAsync(ObjectPath, _serviceGuid.ToString(), sdpOptions);
+        Console.WriteLine("SDP Profile registered successfully. Waiting for connections...");
+    }
+    
+    public Task NewConnectionAsync(ObjectPath device, CloseSafeHandle fd, IDictionary<string, object> properties) {
+        var path = device.ToString();
+    
+        // 1. Get the last part of the path (e.g., "dev_AA_BB_CC_DD_EE_FF")
+        var devPart = path[(path.LastIndexOf('/') + 1)..];
+    
+        // 2. Remove the "dev_" prefix and replace underscores with colons
+        var macAddress = devPart
+            .Replace("dev_", "")
+            .Replace("_", ":");
+        
+        Console.WriteLine($"New Connection from device: {device}");
+        try {
+            var handle = fd.DangerousGetHandle();
+
+            var socket = new LinuxSocket(handle.ToInt32());
+            
+            if (_clientConnectedCallbacks.TryGetValue(macAddress, out var callback)) {
+                callback(macAddress, socket);
+            }
+            else {
+                OnServerConnected?.Invoke(macAddress, socket);
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine($"Error wrapping Bluetooth socket: {ex.Message}");
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task RequestDisconnectionAsync(ObjectPath device) {
+        Console.WriteLine($"Requesting Disconnection from device: {device}");
+        return Task.CompletedTask;
+    }
+
+    public Task ReleaseAsync() {
+        Console.WriteLine("Dbus releasing object");
+        return  Task.CompletedTask;
+    }
+}
