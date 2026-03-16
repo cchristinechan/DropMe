f4291fd5dc1e7f1682826d7a041a494374fae642
roberta1 <roberta1@tcd.ie>
2026-03-16 09:39:40 +0000

    Added permissions service

---
+++ DropMe.Android/Services/AndroidPermissionsService.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading.Tasks;
+using Android;
+using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
+using DropMe.Services;
+
+namespace DropMe.Android.Services;
+// Correct this for different api levels
+public class AndroidPermissionsService(Activity activity) : IPermissionsService {
+    private Activity _activity = activity;
+    public TaskCompletionSource<bool> BluetoothDiscoverableResponse { get; private set; } = new();
+    public bool HasCameraPermissions => ContextCompat.CheckSelfPermission(_activity, Manifest.Permission.Camera) == Permission.Granted;
+
+    public bool HasBluetoothPermissions {
+        get {
+            if (OperatingSystem.IsAndroidVersionAtLeast(31)) {
+                return ContextCompat.CheckSelfPermission(_activity, Manifest.Permission.BluetoothScan) == Permission.Granted &&
+                    ContextCompat.CheckSelfPermission(_activity, Manifest.Permission.BluetoothConnect) ==
+                    Permission.Granted &&
+                    ContextCompat.CheckSelfPermission(_activity, Manifest.Permission.BluetoothAdvertise) ==
+                    Permission.Granted;
+            }
+            return false;
+        }
+    }
+
+    public Task RequestCameraPermission() {
+        _activity.RequestPermissions([Manifest.Permission.Camera], 0);
+        return Task.CompletedTask;
+    }
+
+    public Task RequestBluetoothPermission() {
+        if (OperatingSystem.IsAndroidVersionAtLeast(31)) {
+            _activity.RequestPermissions([
+                Manifest.Permission.BluetoothScan,
+                Manifest.Permission.BluetoothConnect,
+                Manifest.Permission.BluetoothAdvertise,
+                Manifest.Permission.Camera
+            ], 0);
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task RequestBluetoothDiscoverablePermission(int maxDuration) {
+        BluetoothDiscoverableResponse = new();
+        var intent = new Intent(BluetoothAdapter.ActionRequestDiscoverable);
+        intent.PutExtra(BluetoothAdapter.ExtraDiscoverableDuration, maxDuration);
+        _activity.StartActivityForResult(intent, MainActivity.BLUETOOTH_DISCOVERABLE_RQ);
+        return BluetoothDiscoverableResponse.Task;
+    }
+}
