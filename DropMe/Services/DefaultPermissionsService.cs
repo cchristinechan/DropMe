4f2b6566e284dde98d223122deec25cd7b7619a0
roberta1 <roberta1@tcd.ie>
2026-03-16 09:39:40 +0000

    Added permissions service

--- DropMe/Services/DefaultPermissionsService.cs
+++ DropMe/Services/DefaultPermissionsService.cs
@@ -1,17 +1,19 @@
+using System.Threading.Tasks;
+
 namespace DropMe.Services;
 
 public class DefaultPermissionsService : IPermissionsService {
     public bool HasCameraPermissions => true;
     public bool HasBluetoothPermissions => true;
-    public void RequestCameraPermission() {
-
+    public Task RequestCameraPermission() {
+        return Task.CompletedTask;
     }
 
-    public void RequestBluetoothPermission() {
-
+    public Task RequestBluetoothPermission() {
+        return Task.CompletedTask;
     }
 
-    public void RequestBluetoothDiscoverablePermission(int maxDuration) {
-
+    public Task RequestBluetoothDiscoverablePermission(int maxDuration) {
+        return Task.CompletedTask;
     }
 }
